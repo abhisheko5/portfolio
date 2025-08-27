e6e8d463f65d4f9c8bc06da6e125c56696758d4e
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-08-27 19:30:07 +0530

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import About from './Components/About';
-import MyWork from './Components/Mywork';
+import MyWork from './Components/MyWork';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
